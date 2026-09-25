609eb86431e0142d08e1deee089ed36d3bf9c440
Livia <melgacolivia@gmail.com>
2026-09-25 18:44:45 -0300

    restaura versao original do teste04_simu_mc.R

--- teste04_simu_mc.R
+++ teste04_simu_mc.R
@@ -19,15 +19,12 @@ set.seed(222)
 ##### PARAMETROS #####
 
 n     <- 200          # tamanho de cada série simulada
+R     <- 100           # número de réplicas de Monte Carlo (reduza p/ testar mais rápido)
 beta0 <- 1
 beta1 <- 0.5
 beta2 <- -0.1
 phis  <- c(0.3, 0.8)   # cenários de phi a comparar
 
-# número de réplicas de Monte Carlo, um valor DIFERENTE por cenário de phi
-# (nome do vetor = valor de phi em texto, pra buscar depois por "as.character(phi_val)")
-Rs <- setNames(c(25, 50), as.character(phis))
-# Rs[["0.3"]] == 1000   |   Rs[["0.8"]] == 5000
 
 
 ##### FUNÇAO GERADORA GLARMA ######
@@ -95,16 +92,14 @@ resultados <- list()
 
 for (phi_val in phis) {
   
-  R_val <- Rs[[as.character(phi_val)]]  # número de réplicas DESSE phi
+  cat("Rodando Monte Carlo para phi =", phi_val, "\n")
   
-  cat("Rodando Monte Carlo para phi =", phi_val, "com R =", R_val, "réplicas\n")
-  
-  estimativas <- matrix(NA_real_, nrow = R_val, ncol = 4)
+  estimativas <- matrix(NA_real_, nrow = R, ncol = 4)
   colnames(estimativas) <- c("beta0", "beta1", "beta2", "phi")
   
-  for (r in 1:R_val) {
+  for (r in 1:R) {
     estimativas[r, ] <- simula_e_ajusta(n, beta0, beta1, beta2, phi_val)
-    if (r %% 50 == 0) cat("  réplica", r, "de", R_val, "\n") #tipo uma barra de progressão, a cada 50 réplicas
+    if (r %% 50 == 0) cat("  réplica", r, "de", R, "\n") #tipo uma barra de progressão, a cada 50 réplicas
   }
   
   resultados[[as.character(phi_val)]] <- estimativas
@@ -116,9 +111,8 @@ tabela_larga <- data.frame(parametro = c("beta0", "beta1", "beta2", "phi"))
 
 for (phi_val in phis) {
   
-  R_val <- Rs[[as.character(phi_val)]]
-  est   <- resultados[[as.character(phi_val)]]
-  vt    <- c(beta0, beta1, beta2, phi_val)  # valor verdadeiro de cada parâmetro nesse cenário
+  est <- resultados[[as.character(phi_val)]]
+  vt  <- c(beta0, beta1, beta2, phi_val)  # valor verdadeiro de cada parâmetro nesse cenário
   
   #filtrando as NA's do calculo e da tabela
   est_validas <- est[stats::complete.cases(est), , drop = FALSE] 
@@ -130,7 +124,7 @@ for (phi_val in phis) {
   tabela_larga[[paste0("media_phi", phi_val)]] <- round(medias, 4)
   tabela_larga[[paste0("eqm_phi",   phi_val)]] <- round(eqm, 4)
   
-  cat("phi =", phi_val, "->", n_validas, "de", R_val, "réplicas convergiram\n")
+  cat("phi =", phi_val, "->", n_validas, "de", R, "réplicas convergiram\n")
 }
 
 ##### TABELA E GRAFICO FINAL #####
@@ -147,10 +141,10 @@ par(mfrow = c(1, 3))
 for (i in 1:3) {
   boxplot(
     resultados[["0.3"]][, i], resultados[["0.8"]][, i],
-    names = c("phi = 0.3", "phi = 0.8"),
-    main = colnames(estimativas)[i]
-  )
-  abline(h = c(beta0, beta1, beta2)[i], col = "red", lty = 2)
+     names = c("phi = 0.3", "phi = 0.8"),
+     main = colnames(estimativas)[i]
+   )
+   abline(h = c(beta0, beta1, beta2)[i], col = "red", lty = 2)
 }
 
 
